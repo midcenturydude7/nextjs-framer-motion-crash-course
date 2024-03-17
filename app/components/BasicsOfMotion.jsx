ecb2ab110ed90a1140d6576e148d82b88dfb4ebc
midCenturyDude7 <mgriffes@gmail.com>
2024-03-17 13:21:12 -0700

    Set up first component and loaded framer-motion

---
+++ app/components/BasicsOfMotion.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+export default function BasicsOfMotion() {
+  return (
+    <div
+      style={{
+        diplay: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}>
+      Stuff will go here
+    </div>
+  );
+}
