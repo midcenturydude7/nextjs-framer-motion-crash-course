545529f269e7e6a664517de39c7197edc5d5b2c0
midCenturyDude7 <mgriffes@gmail.com>
2024-03-17 14:11:45 -0700

    Set up Gestures component

---
+++ app/components/Gestures.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Gestures() {
+  return (
+    <div>
+      <div>Stuff will go here</div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import BasicsOfMotion from "./components/BasicsOfMotion";
+import Gestures from "./components/Gestures";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <BasicsOfMotion />
+      {/* <BasicsOfMotion /> */}
+      <Gestures />
     </div>
   );
 }
